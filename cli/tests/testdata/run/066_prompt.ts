60c59c5c10ac868a770e25b4c08582c311f6e908
lionel-rowe <lionel.rowe@gmail.com>
2023-03-16 21:25:50 +0800

    fix(runtime): Make alert, prompt, confirm keyboard interaction consistent vs browsers

--- cli/tests/testdata/run/066_prompt.ts
+++ cli/tests/testdata/run/066_prompt.ts
@@ -1,21 +1,68 @@
-const name0 = prompt("What is your name?", "Jane Doe"); // Answer John Doe
-console.log(`Your name is ${name0}.`);
-const name1 = prompt("What is your name?", "Jane Doe"); // Answer with default
-console.log(`Your name is ${name1}.`);
-const input = prompt(); // Answer foo
-console.log(`Your input is ${input}.`);
-const answer0 = confirm("Question 0"); // Answer y
-console.log(`Your answer is ${answer0}`);
-const answer1 = confirm("Question 1"); // Answer n
-console.log(`Your answer is ${answer1}`);
-const answer2 = confirm("Question 2"); // Answer with yes (returns false)
-console.log(`Your answer is ${answer2}`);
-const answer3 = confirm(); // Answer with default
-console.log(`Your answer is ${answer3}`);
-const windows = prompt("What is Windows EOL?");
-console.log(`Your answer is ${JSON.stringify(windows)}`);
+// regenerate Rust code:
+// const x = await Deno.readTextFile('cli/tests/testdata/run/066_prompt.ts')
+// console.info(x.split('\n').filter(line => /^\/\/ (Input|Output)\(/.test(line)).join('\n'))
+
+// TODO: fix this for new logic/new tests; see `_066_prompt` in `cli/tests/integration/run_tests.rs` (line 2435)
+
 alert("Hi");
+// Output("Hi [Enter]\r\n"),
 alert();
-console.log("The end of test");
-const eof = prompt("What is EOF?");
-console.log(`Your answer is ${JSON.stringify(eof)}`);
+// Output("Alert [Enter]\r\n"),
+
+const userInput = prompt();
+// Input("foo\n"),
+console.log(`Your input is ${userInput}.`);
+// Output("Your input is foo.\r\n"),
+const enterDefault = prompt("What is your name?", "Jane Doe");
+// Output("What is your name? [Jane Doe]\r\n"),
+// Input("\n"),
+console.log(`Your name is ${enterDefault}.`);
+// Output("Your name is Jane Doe.\r\n"),
+const changedDefault = prompt("What is your name?", "Jane Doe");
+// Output("What is your name? [Jane Doe]\r\n"),
+// Input("John Doe\n"),
+console.log(`Your name is ${changedDefault}.`);
+// Output("Your name is John Doe.\r\n"),
+const enterLinux = prompt("Press enter on Linux");
+// Output("Press enter on Linux\r\n"),
+console.log(`Your answer is ${JSON.stringify(enterLinux)}`);
+// Output("Your answer is \"\"\r\n"),
+const enterWin = prompt("Press enter on Windows");
+// Output("Press enter on Windows\r\n"),
+console.log(`Your answer is ${JSON.stringify(enterWin)}`);
+// Output("Your answer is \"\"\r\n"),
+const esc = prompt("Press esc");
+// Output("Press esc"),
+// Input("\x1b"),
+console.log(`Your answer is ${JSON.stringify(esc)}`);
+// Output("Your answer is null\r\n"),
+
+const answerY = confirm("Yes or no?");
+// Output("Yes or no? [Y/n]\r\n"),
+// Input("Y\n"),
+console.log(`Your answer is ${answerY}`);
+const answerN = confirm("Yes or no?");
+// Output("Yes or no? [Y/n]\r\n"),
+// Input("N\n"),
+console.log(`Your answer is ${answerN}`);
+// Output("Your answer is false\r\n"),
+const answerNo = confirm("Yes or no?");
+// Output("Yes or no? [Y/n]\r\n"),
+// Input("no\n"),
+console.log(`Your answer is ${answerNo}`);
+// Output("Your answer is true\r\n"),
+const answerArbitrary = confirm("Yes or no?");
+// Output("Yes or no? [Y/n]\r\n"),
+// Input("abcxyz\n"),
+console.log(`Your answer is ${answerArbitrary}`);
+// Output("Your answer is true\r\n"),
+const answerEnterDefault = confirm();
+// Output("Confirm [Y/n]\r\n"),
+// Input("\n"),
+console.log(`Your answer is ${answerEnterDefault}`);
+// Output("Your answer is true\r\n"),
+const answerEsc = confirm();
+// Output("Confirm [Y/n]\r\n"),
+// Input("\x1b"),
+console.log(`Your answer is ${answerEsc}`);
+// Output("Your answer is false\r\n"),
--- runtime/js/41_prompt.js
+++ runtime/js/41_prompt.js
@@ -3,19 +3,36 @@ const core = globalThis.Deno.core;
 const primordials = globalThis.__bootstrap.primordials;
 import { isatty } from "internal:runtime/40_tty.js";
 import { stdin } from "internal:deno_io/12_io.js";
-const { ArrayPrototypePush, StringPrototypeCharCodeAt, Uint8Array } =
-  primordials;
+const {
+  ArrayPrototypeFill,
+  ArrayPrototypeJoin,
+  ArrayPrototypePop,
+  ArrayPrototypePush,
+  StringPrototypeCharCodeAt,
+  Uint8Array
+} = primordials;
 const LF = StringPrototypeCharCodeAt("\n", 0);
 const CR = StringPrototypeCharCodeAt("\r", 0);
+const SP = StringPrototypeCharCodeAt(" ", 0);
+const HYPHEN = StringPrototypeCharCodeAt("-", 0);
+const ETX = 0x03; // Ctrl+C
+const EOT = 0x04; // Ctrl+D
+const ESC = 0x1b;
+const DEL = 0x7f; // Backspace
+const ETB = 0x17; // Ctrl+Backspace (Unix)
+const BS = 0x08; // Ctrl+Backspace (Windows)
+
+const LINE_UP = "\x1b[1A";
+const LINE_CLEAR = "\x1b[2K";
+const CYAN = "\x1b[36m";
+const CLEAR_FMT = "\x1b[39m";
 
 function alert(message = "Alert") {
   if (!isatty(stdin.rid)) {
     return;
   }
 
-  core.print(`${message} [Enter] `, false);
-
-  readLineFromStdinSync();
+  readLineFromStdinSync(`${message} [Enter]`);
 }
 
 function confirm(message = "Confirm") {
@@ -23,54 +40,136 @@ function confirm(message = "Confirm") {
     return false;
   }
 
-  core.print(`${message} [y/N] `, false);
+  const answer = readLineFromStdinSync(`${message} [Y/n]`);
 
-  const answer = readLineFromStdinSync();
-
-  return answer === "Y" || answer === "y";
+  return answer != null && answer !== "n" && answer !== "N";
 }
 
-function prompt(message = "Prompt", defaultValue) {
-  defaultValue ??= null;
-
+function prompt(message = "Prompt", defaultValue = "") {
   if (!isatty(stdin.rid)) {
     return null;
   }
 
-  core.print(`${message} `, false);
-
-  if (defaultValue) {
-    core.print(`[${defaultValue}] `, false);
-  }
-
-  return readLineFromStdinSync() || defaultValue;
+  return readLineFromStdinSync(message, defaultValue);
 }
 
-function readLineFromStdinSync() {
-  const c = new Uint8Array(1);
-  const buf = [];
+function readLineFromStdinSync(promptText, defaultValue = "") {
+  stdin.setRaw(true);
+
+  promptText = formatPrompt(promptText)
+  const buf = [...core.encode(defaultValue)];
+  let canceled = false;
+  let prevPhysicalLines = wrap(promptText).length;
 
   while (true) {
+    prevPhysicalLines = writeLine(promptText, buf, prevPhysicalLines);
+
+    const c = new Uint8Array(4);
     const n = stdin.readSync(c);
-    if (n === null || n === 0) {
+
+    if (n === null || n === 0 || byteMatch(c, [CR]) || byteMatch(c, [LF]) || byteMatch(c, [CR, LF])) {
       break;
     }
-    if (c[0] === CR) {
-      const n = stdin.readSync(c);
-      if (c[0] === LF) {
-        break;
-      }
-      ArrayPrototypePush(buf, CR);
-      if (n === null || n === 0) {
-        break;
-      }
-    }
-    if (c[0] === LF) {
+    if (byteMatch(c, [ESC]) || byteMatch(c, [ETX]) || byteMatch(c, [EOT])) {
+      canceled = true;
       break;
     }
-    ArrayPrototypePush(buf, c[0]);
+    if (byteMatch(c, [DEL])) {
+      while (buf.length) {
+        const byte = ArrayPrototypePop(buf);
+        if (isFirstByteOfUtf8Char(byte)) {
+          break;
+        }
+      }
+    } else if (byteMatch(c, [ETB]) || byteMatch(c, [BS])) {
+      while (buf.length) {
+        ArrayPrototypePop(buf);
+        const prevByte = buf[buf.length - 1];
+        if (prevByte === SP || prevByte === HYPHEN) {
+          break;
+        }
+      }
+      prevPhysicalLines = writeLine(promptText, buf, prevPhysicalLines);
+    } else {
+      const utf8ByteLength = getUtf8ByteLength(c[0]);
+
+      if (utf8ByteLength === 1 && (c[1] || c[2] || c[3])) {
+        // is control char
+        continue;
+      }
+
+      for (let i = 0; i < utf8ByteLength; ++i) {
+        ArrayPrototypePush(buf, c[i]);
+      }
+    }
   }
-  return core.decode(new Uint8Array(buf));
+
+  core.print("\n", false);
+
+  stdin.setRaw(false);
+  return canceled ? null : core.decode(new Uint8Array(buf));
+}
+
+function getUtf8ByteLength(firstByte) {
+  if ((firstByte & 0x80) === 0x00) return 1;
+  if ((firstByte & 0xE0) === 0xC0) return 2;
+  if ((firstByte & 0xF0) === 0xE0) return 3;
+  if ((firstByte & 0xF8) === 0xF0) return 4;
+
+  return -1;
+}
+
+function isFirstByteOfUtf8Char(byte) {
+  return (byte & 0xC0) !== 0x80;
+}
+
+function byteMatch(actualBytes, bytesToMatch) {
+  for (let i = 0; i < bytesToMatch.length; ++i) {
+    if (actualBytes[i] !== bytesToMatch[i]) return false;
+  }
+
+  for (let i = bytesToMatch.length; i < 4; ++i) {
+    if (actualBytes[i]) return false;
+  }
+
+  return true;
+}
+
+function formatPrompt(str) {
+  return `${CYAN}${str}${CLEAR_FMT} `;
+}
+
+function wrap(str) {
+  const { columns } = consoleSize();
+
+  if (!columns) return [""];
+
+  const wrapped = [""];
+  const words = StringPrototypeSplit(str, /(?<=[ -])/);
+
+  for (let word of words) {
+    if (wrapped[wrapped.length - 1].length + word.length <= columns) {
+      wrapped[wrapped.length - 1] += word;
+    } else if (word.length <= columns) {
+      ArrayPrototypePush(wrapped, word);
+    } else {
+      while (word.length) {
+        ArrayPrototypePush(wrapped, StringPrototypeSlice(word, 0, columns));
+        word = StringPrototypeSlice(word, columns);
+      }
+    }
+  }
+
+  return wrapped;
+}
+
+function writeLine(promptText, buf, prevPhysicalLines) {
+  const wrapped = wrap(`${promptText}${core.decode(new Uint8Array(buf))}`);
+  const clear = ArrayPrototypeJoin(ArrayPrototypeFill(new Array(prevPhysicalLines), LINE_CLEAR), LINE_UP);
+
+  core.print(`${clear}\r${ArrayPrototypeJoin(wrapped, "\n")}`, false);
+
+  return wrapped.length;
 }
 
 export { alert, confirm, prompt };
